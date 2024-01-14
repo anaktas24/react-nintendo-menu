b2359b2c1637780d496c145c639f4f8f9f1b903c
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-14 21:55:28 +0100

    Added first component

--- pizza menu/src/App.jsx
+++ pizza menu/src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <>
       <div>
-       <h1>Hello</h1>
+       <h1>Menu</h1>
+       <h2>Pizzas:</h2>
        <Pizza/>
+       <Pizza2/>
       </div>
       <h1>Homepage</h1>
     </>
@@ -20,6 +22,15 @@ function Pizza(){
       <img src="../pizza_diavola.jpeg" alt="pizza"/>
       <h1>Pizza Diavola</h1>
       <p>Pepperoni, Tomato, Mozarella, Chilli, Onion</p>
+    </div>
+  )
+}
+
+function Pizza2(){
+  return(
+    <div>
+      <h1>Pizza Margarita</h1>
+      <p>Mozzarella, Tomato</p>
     </div>
   )
 }
