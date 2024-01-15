0e53ff874c94e8e25a96cba7b669cfaac30551ab
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-15 18:57:33 +0100

    first styling of components

--- pizza menu/src/App.jsx
+++ pizza menu/src/App.jsx
@@ -16,29 +16,44 @@ function App() {
 }
 
 function Header(){
-  return <h1>React Pizza Co.</h1>
+  const style = {}
+  //const style = {color: 'green', fontSize: '60px', textTransform: 'uppercase'}
+  return (
+    <header className="header">
+      <h1 style={style}>React Pizza Co.</h1>
+    </header>
+  )
 }
+
+
 
 function Menu(){
   return (
-    <div>
+    <main className='menu'>
       <h2>Our menu</h2>
       <Pizza/>
       <Pizza/>
       <Pizza2/>
-    </div>
+    </main>
   )
 }
 
 function Footer(){
-  return <footer>{new Date().toLocaleTimeString()}. Were currently open</footer>
+  const hour = new Date().getHours()
+  const openHours = 12
+  const closeHours = 22
+  const isOpen = hour >= openHours && hour <= closeHours
+  console.log(isOpen)
+
+
+  return <footer className='footer'>{new Date().toLocaleTimeString()}. Were currently open</footer>
 }
 
 function Pizza(){
   return(
     <div>
       <img src="../pizza_diavola.jpeg" alt="pizza"/>
-      <h1>Pizza Diavola</h1>
+      <h3>Pizza Diavola</h3>
       <p>Pepperoni, Tomato, Mozarella, Chilli, Onion</p>
     </div>
   )
@@ -47,7 +62,7 @@ function Pizza(){
 function Pizza2(){
   return(
     <div>
-      <h1>Pizza Margarita</h1>
+      <h3>Pizza Margarita</h3>
       <p>Mozzarella, Tomato</p>
     </div>
   )
